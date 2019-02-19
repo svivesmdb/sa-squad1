90559c294c20eb31c6af61982a9cfcaa6a53744b
Sergi Vives <sergivives@MacBook-Pro-de-Sergi.local>
2019-02-19 13:23:58 +0100

    Pushing latest version of the dao

--- dao.py
+++ dao.py
@@ -1,46 +1,52 @@
 
 from pymongo import MongoClient
+import gridfs
 
 # Set your classes here.
 
 
 class DAO():
 
-    client = -1
-
     def __init__(self, url):
         self.client = MongoClient(url)
         self.db = self.client.proofPointsManager
+        self.fs = gridfs.GridFS(self.db)
     
     def addProofPoint(self, company, industry, project, project_desc, use_case, creation_date, owner_id):
-        
         pp = {
-            "customerCompany":company,
-            "customerIndustry":industry,
+            "customerCompany": company,
+            "customerIndustry": industry,
             "customerProject": project,
             "customerProjectDescription": project_desc,
-            "useCase":use_case,
+            "useCase": use_case,
             "creationDate": creation_date,
             "owner_id": owner_id
         }
-
         self.db.proofPoints.insert_one(pp)
 
-    def getProofPoints(self):
-        cursor = self.db.proofPoints.find({})
+    def cursorToArray(self,cursor):
         docs = []
         for doc in cursor:
             docs.append(doc)
         return docs
 
+    def getProofPoints(self):
+        return self.cursorToArray(self.db.proofPoints.find({}))
+
     def getProofPoint(self, id):
-        return self.db.proofPoints.find({"_id" : id})
+        return self.db.proofPoints.findOne({"_id" : id})
     
     def downloadProofPoint(self, id):
-        return self.db.proofPoints.find({"_id" : id})
+        return self.db.proofPoints.findOne({"_id" : id})
     
-    def searchProofPoints(self):
-        return self.db.proofPoints.find({})
+    def searchProofPoints(self, keywords):
+        res = self.db.proofPoints.find({
+            '$text':{
+                '$search': "\"" + keywords+  "\"" 
+            }
+        })
+
+        return self.cursorToArray(res)
     
     def getProofPointUrl(self):
 
